d705df09aab2413f4b8299fa35eaae5b4ed5cc90
Иван <155732866+Doczadrot@users.noreply.github.com>
2025-09-21 03:58:42 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
 
-# Ждем доступности базы данных
 echo "Waiting for postgres..."
 while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
   sleep 0.1
 done
 echo "PostgreSQL started"
 
-# Ждем доступности Redis
+# Ждем доступности редис
 echo "Waiting for redis..."
 while ! nc -z redis 6379; do
   sleep 0.1
 done
 echo "Redis started"
 
-# Создаем директорию для логов
+# Создаем парку /app/logs
 mkdir -p /app/logs
 
 # Выполняем миграции
